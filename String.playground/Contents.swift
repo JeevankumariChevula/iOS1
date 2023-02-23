2b2831c13c103f144eb1ecbbc7ed90cafb1ad7cd
JeevankumariChevula <111479462+JeevankumariChevula@users.noreply.github.com>
2023-02-23 08:56:44 -0600

    Add files via upload

---
+++ String.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var fact = "Want to become an iOS developer"
+var dev = "practice everyday"
+var author = "Jeevan Kumari Chevula"
+
+print(fact.count)
+fact += " ,it is better than yesterday"
+print(fact)
+
+fact.append(" may be")
+print(fact)
+
+author.lowercased()
+
+author.uppercased()
+
+
