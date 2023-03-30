9feab521e605f427e18babba77b0a80ffe19101a
JeevankumariChevula <111479462+JeevankumariChevula@users.noreply.github.com>
2023-03-29 23:17:38 -0500

    Work In Progress

---
+++ Chevula_WordGuess/Chevula_WordGuess/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Chevula_WordGuess
+//
+//  Created by Chevula,Jeevan Kumari on 3/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var wordsGuessedLabel: UILabel!
+    
+    
+    @IBOutlet weak var wordsRemainingLabel: UILabel!
+    
+    
+    @IBOutlet weak var totalWordsLabel: UILabel!
+    
+    
+
+    @IBOutlet weak var userGuessLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+}
+
